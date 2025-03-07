2cd2531a52d0d3364b36a1a0d7f58cc71f6b1758
Jimmy Flatting <jimmy.flatting@knowit.se>
2025-03-07 07:57:43 +0100

    fix: show dotfiles

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,173 +3,180 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup {}
-		end,
-	},
-	{ "christoomey/vim-tmux-navigator" }, -- Navigation between splits
-	{ 'github/copilot.vim' },      -- CoPilot
-	-- avante.nvim
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		version = false, -- set this if you want to always pull the latest change
-		opts = {
-			-- add any opts here
-			provider = "copilot",
-			auto_suggestions_provider = "copilot",
-			behaviour = {
-				auto_suggestions = false, -- Experimental stage
-				auto_set_highlight_group = true,
-				auto_set_keymaps = true,
-				auto_apply_diff_after_generation = false,
-				support_paste_from_clipboard = false,
-			},
-			vendors = {
-				---@type AvanteProvider
-				ollama = {
-					["local"] = true,
-					endpoint = "127.0.0.1:11434/v1",
-					model = "llama3.2",
-					parse_curl_args = function(opts, code_opts)
-						return {
-							url = opts.endpoint .. "/chat/completions",
-							headers = {
-								["Accept"] = "application/json",
-								["Content-Type"] = "application/json",
-							},
-							body = {
-								model = opts.model,
-								messages = require("avante.providers").copilot
-								.parse_message(code_opts),  -- you can make your own message, but this is very advanced
-								max_tokens = 2048,
-								stream = true,
-							},
-						}
-					end,
-					parse_response_data = function(data_stream, event_state, opts)
-						require("avante.providers").openai.parse_response(data_stream,
-							event_state, opts)
-					end,
-				},
-			},
-			mappings = {
-				--- @class AvanteConflictMappings
-				diff = {
-					ours = "co",
-					theirs = "ct",
-					all_theirs = "ca",
-					both = "cb",
-					cursor = "cc",
-					next = "]x",
-					prev = "[x",
-				},
-				suggestion = {
-					accept = "<M-l>",
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-				jump = {
-					next = "]]",
-					prev = "[[",
-				},
-				submit = {
-					normal = "<CR>",
-					insert = "<C-s>",
-				},
-				sidebar = {
-					apply_all = "A",
-					apply_cursor = "a",
-					switch_windows = "<Tab>",
-					reverse_switch_windows = "<S-Tab>",
-				},
-			},
-			hints = { enabled = true },
-			windows = {
-				---@type "right" | "left" | "top" | "bottom"
-				position = "right", -- the position of the sidebar
-				wrap = true, -- similar to vim.o.wrap
-				width = 30, -- default % based on available width
-				sidebar_header = {
-					enabled = false, -- true, false to enable/disable the header
-					align = "center", -- left, center, right for title
-					rounded = true,
-				},
-				input = {
-					prefix = "> ",
-				},
-				edit = {
-					border = "rounded",
-					start_insert = true, -- Start insert mode when opening the edit window
-				},
-				ask = {
-					floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-					start_insert = true, -- Start insert mode when opening the ask window, only effective if floating = true.
-					border = "rounded",
-				},
-			},
-			highlights = {
-				---@type AvanteConflictHighlights
-				diff = {
-					current = "DiffText",
-					incoming = "DiffAdd",
-				},
-			},
-			--- @class AvanteConflictUserConfig
-			diff = {
-				autojump = true,
-				---@type string | fun(): any
-				list_opener = "copen",
-			},
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				'MeanderingProgrammer/render-markdown.nvim',
-				config = function()
-					require('render-markdown').setup({})
-				end,
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
-	}
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+  { 'christoomey/vim-tmux-navigator' }, -- Navigation between splits
+  { 'github/copilot.vim' }, -- CoPilot
+  -- avante.nvim
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = 'copilot',
+      auto_suggestions_provider = 'copilot',
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+      },
+      vendors = {
+        ---@type AvanteProvider
+        ollama = {
+          ['local'] = true,
+          endpoint = '127.0.0.1:11434/v1',
+          model = 'llama3.2',
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint .. '/chat/completions',
+              headers = {
+                ['Accept'] = 'application/json',
+                ['Content-Type'] = 'application/json',
+              },
+              body = {
+                model = opts.model,
+                messages = require('avante.providers').copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+                max_tokens = 2048,
+                stream = true,
+              },
+            }
+          end,
+          parse_response_data = function(data_stream, event_state, opts)
+            require('avante.providers').openai.parse_response(data_stream, event_state, opts)
+          end,
+        },
+      },
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = 'co',
+          theirs = 'ct',
+          all_theirs = 'ca',
+          both = 'cb',
+          cursor = 'cc',
+          next = ']x',
+          prev = '[x',
+        },
+        suggestion = {
+          accept = '<M-l>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+        jump = {
+          next = ']]',
+          prev = '[[',
+        },
+        submit = {
+          normal = '<CR>',
+          insert = '<C-s>',
+        },
+        sidebar = {
+          apply_all = 'A',
+          apply_cursor = 'a',
+          switch_windows = '<Tab>',
+          reverse_switch_windows = '<S-Tab>',
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        ---@type "right" | "left" | "top" | "bottom"
+        position = 'right', -- the position of the sidebar
+        wrap = true, -- similar to vim.o.wrap
+        width = 30, -- default % based on available width
+        sidebar_header = {
+          enabled = false, -- true, false to enable/disable the header
+          align = 'center', -- left, center, right for title
+          rounded = true,
+        },
+        input = {
+          prefix = '> ',
+        },
+        edit = {
+          border = 'rounded',
+          start_insert = true, -- Start insert mode when opening the edit window
+        },
+        ask = {
+          floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+          start_insert = true, -- Start insert mode when opening the ask window, only effective if floating = true.
+          border = 'rounded',
+        },
+      },
+      highlights = {
+        ---@type AvanteConflictHighlights
+        diff = {
+          current = 'DiffText',
+          incoming = 'DiffAdd',
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        ---@type string | fun(): any
+        list_opener = 'copen',
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        config = function()
+          require('render-markdown').setup {}
+        end,
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }
